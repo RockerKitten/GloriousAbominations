f97c7ac4eaca9ff76bcc7c3ca63c0c52f6588df8
RockerKitten <84648053+RockerKitten@users.noreply.github.com>
2021-06-11 04:03:27 -0500

    boo yah! I DID THE THING!!!

--- JotunnModStub/Properties/AssemblyInfo.cs
+++ JotunnModStub/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GloriousAbominations")]
+[assembly: AssemblyTitle("AllTheExtras")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GloriousAbominations")]
-[assembly: AssemblyCopyright("Copyright RockerKittne and plumga ©  2021")]
+[assembly: AssemblyProduct("AllTheExtras")]
+[assembly: AssemblyCopyright("Copyright RockerKitten ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
