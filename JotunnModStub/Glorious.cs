a8d7238d40793175c192b0e058ece39d02e2631a
RockerKitten <84648053+RockerKitten@users.noreply.github.com>
2021-06-13 02:48:44 -0500

    name changes

--- JotunnModStub/Glorious.cs
+++ JotunnModStub/Glorious.cs
@@ -9,22 +9,27 @@ using Jotunn.Managers;
 using System;
 
 
-namespace GloriousAbomination
+namespace Glorious
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid, "2.0.12")]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class GloriousAbominations : BaseUnityPlugin
+    internal class Glorious : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.odinplus.gloriousabominations";
-        public const string PluginName = "GloriousAbominations";
-        public const string PluginVersion = "0.0.1";
+        public const string PluginGUID = "odinplus.glorious";
+        public const string PluginName = "Glorious";
+        public const string PluginVersion = "1.0.0";
         public AssetBundle assetBundle;
+        public AssetBundle assetWigs;
+        public AssetBundle assetRainbow;
         public EffectList buildStone;
         public EffectList buildWood;
         public EffectList buildTorch;
+        public EffectList fxcraft;
+        public EffectList fxdone;
+        public EffectList fxrepair;
         public AudioSource torchVol;
-        public AudioSource bonfireVol;
+        //public AudioSource bonfireVol;
         public EffectList breakStone;
         public EffectList breakWood;
         public EffectList breakTorch;
@@ -34,14 +39,16 @@ namespace GloriousAbomination
         public CustomItem wigC;
         public GameObject wigMohawkPonyFab;
         public CustomItem wigMohawkPony;
+        public GameObject sfxcraft;
+        public GameObject sfxcraftdone;
+        public GameObject sfxrepair;
+        
 
 
         private void Awake()
         {
 
             AssetLoad();
-            LoadWigClaire();
-            LoadWigMohawkPony();
 
             ItemManager.OnVanillaItemsAvailable += LoadSounds;
 
@@ -49,7 +56,8 @@ namespace GloriousAbomination
         private void AssetLoad()
         {
             assetBundle = AssetUtils.LoadAssetBundleFromResources("misc", Assembly.GetExecutingAssembly());
-            assetBundle = AssetUtils.LoadAssetBundleFromResources("rainbowwigs", Assembly.GetExecutingAssembly());
+            assetWigs= AssetUtils.LoadAssetBundleFromResources("rainbowwigs", Assembly.GetExecutingAssembly());
+            assetRainbow = AssetUtils.LoadAssetBundleFromResources("rainbows", Assembly.GetExecutingAssembly());
         }
         private void LoadSounds()
         {
@@ -67,9 +75,14 @@ namespace GloriousAbomination
                 var vfxstonebreak = PrefabManager.Cache.GetPrefab<GameObject>("vfx_RockDestroyed");
                 var vfxbreak = PrefabManager.Cache.GetPrefab<GameObject>("vfx_SawDust");
                 var sfxstonehit = PrefabManager.Cache.GetPrefab<GameObject>("sfx_rock_hit");
+                var sfxcraft = PrefabManager.Cache.GetPrefab<GameObject>("sfx_gui_craftitem_workbench");
+                var sfxcraftdone = PrefabManager.Cache.GetPrefab<GameObject>("sfx_gui_craftitem_workbench_end"); //(craft item done
+                var sfxrepair = PrefabManager.Cache.GetPrefab<GameObject>("sfx_gui_repairitem_workbench"); //enabled = true repair item effect
 
 
-
+                fxrepair = new EffectList { m_effectPrefabs = new EffectList.EffectData[1] { new EffectList.EffectData { m_prefab = sfxrepair } } };
+                fxdone = new EffectList { m_effectPrefabs = new EffectList.EffectData[1] { new EffectList.EffectData { m_prefab = sfxcraftdone } } };
+                fxcraft = new EffectList { m_effectPrefabs = new EffectList.EffectData[1] { new EffectList.EffectData { m_prefab = sfxcraftdone } } };
                 buildStone = new EffectList { m_effectPrefabs = new EffectList.EffectData[2] { new EffectList.EffectData { m_prefab = sfxstone }, new EffectList.EffectData { m_prefab = vfxstone } } };
                 buildWood = new EffectList { m_effectPrefabs = new EffectList.EffectData[2] { new EffectList.EffectData { m_prefab = sfxwood }, new EffectList.EffectData { m_prefab = vfxwood } } };
                 buildTorch = new EffectList { m_effectPrefabs = new EffectList.EffectData[2] { new EffectList.EffectData { m_prefab = sfxmetal }, new EffectList.EffectData { m_prefab = vfxmetal } } };
@@ -84,9 +97,15 @@ namespace GloriousAbomination
 
 
                 LoadTorch();
-                
-                torchVol.outputAudioMixerGroup = AudioMan.instance.m_ambientMixer;
-                bonfireVol.outputAudioMixerGroup = AudioMan.instance.m_ambientMixer;
+                LoadWigClaire();
+                LoadWigMohawkPony();
+                LoadRainbowWorkbench();
+                LoadRainbowHammer();
+                LoadRainbowBanner();
+
+
+                //torchVol.outputAudioMixerGroup = AudioMan.instance.m_ambientMixer;
+                //bonfireVol.outputAudioMixerGroup = AudioMan.instance.m_ambientMixer;
 
             }
             catch (Exception ex)
@@ -121,12 +140,17 @@ namespace GloriousAbomination
 
                 });
 
-            torchVol = torchFab.AddComponent<AudioSource>();
+            /*torchVol = torchFab.AddComponent<AudioSource>();
             torchVol.clip = assetBundle.LoadAsset<AudioClip>("torch_clip");
             torchVol.playOnAwake = true;
             torchVol.loop = true;
-            torchVol.rolloffMode = AudioRolloffMode.Linear;
-            //if (AudioMan.instance == null) Jotunn.Logger.LogError("lolwut");
+            torchVol.rolloffMode = AudioRolloffMode.Linear;*/
+            if (AudioMan.instance == null) Jotunn.Logger.LogError("failed to load mixer");
+            else
+            {
+                var torchBurn = torchFab.GetComponentInChildren<AudioSource>();
+                torchBurn.outputAudioMixerGroup = AudioMan.instance.m_ambientMixer;
+            }
            
             var torchBreak = torchFab.GetComponent<WearNTear>();
             torchBreak.m_destroyedEffect = breakTorch;
@@ -150,7 +174,7 @@ namespace GloriousAbomination
                     Name = "$claire_kiara_rainbow",
                     Enabled = true,
                     Amount = 1,
-                    CraftingStation = "piece_workbench",
+                    CraftingStation = "rk_rainbowbridge",
                     MinStationLevel = 1,
                     Requirements = new[]
                     {
@@ -168,7 +192,7 @@ namespace GloriousAbomination
                     Name = "$mohawkpony_redearcat_rainbow",
                     Enabled = true,
                     Amount = 1,
-                    CraftingStation = "piece_workbench",
+                    CraftingStation = "rk_rainbowBridge",
                     MinStationLevel = 1,
                     Requirements = new[]
                     {
@@ -176,6 +200,63 @@ namespace GloriousAbomination
                     }
                 });
             ItemManager.Instance.AddItem(wigC);
+        }
+        public void LoadRainbowWorkbench()
+        {
+            var workbenchFab = assetRainbow.LoadAsset<GameObject>("rk_rainbowbridge");
+            var workbench = new CustomPiece(workbenchFab,
+                new PieceConfig
+                {
+                    CraftingStation = "",
+                    AllowedInDungeons = false,
+                    Enabled = true,
+                    PieceTable = "_HammerPieceTable",
+                    Requirements = new[]
+                    {
+                        new RequirementConfig {Item = "Raspberry", Amount = 3, Recover = true},
+                        new RequirementConfig {Item = "Blueberries", Amount = 3, Recover = true},
+                        new RequirementConfig {Item = "Dandelion", Amount = 3, Recover = true}
+                    }
+                });
+            var workbenchBuild = workbenchFab.GetComponent<Piece>();
+            workbenchBuild.m_placeEffect = buildWood;
+
+            var workbenchCraft = workbenchFab.GetComponent<CraftingStation>();
+            workbenchCraft.m_craftItemEffects = fxcraft;
+            workbenchCraft.m_repairItemDoneEffects = fxrepair;
+            workbenchCraft.m_craftItemDoneEffects = fxdone;
+
+            var workbenchBreak = workbenchFab.GetComponent<WearNTear>();
+            workbenchBreak.m_destroyedEffect = breakWood;
+
+            PieceManager.Instance.AddPiece(workbench);
+            
+        }
+        private void LoadRainbowHammer()
+        {
+            var rainbowhammerFab = assetRainbow.LoadAsset<GameObject>("rk_rainbowhammer");
+            var rainbowhammer = new CustomItem(rainbowhammerFab, fixReference: false,
+                new ItemConfig
+                {
+                    Name = "$rainbow_hammer",
+                    Enabled = true,
+                    Amount = 1,
+                    CraftingStation = "rk_rainbowbridge",
+                    MinStationLevel = 1,
+                    Requirements = new[]
+                    {
+                        new RequirementConfig {Item = ("Hammer"), Amount = 1, Recover = true},
+                        new RequirementConfig {Item = ("Blueberies"), Amount = 3, Recover = true},
+                        new RequirementConfig {Item = ("Raspberry"), Amount = 3, Recover = true},
+                        new RequirementConfig {Item = ("Dandelion"), Amount = 3, Recover = true}
+                    }
+
+                });
+            ItemManager.Instance.AddItem(rainbowhammer);
+        }
+        private void LoadRainbowBanner()
+        {
+
         }
     }
 }
